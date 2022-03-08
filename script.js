4eba383712b1bbd3461758b6401b1cfcd3967198
ecoronado <lincicoronado@gmail.com>
2022-03-08 01:50:47 -0500

    trying to figure this out

---
+++ script.js
@@ -0,0 +1,6 @@
+function playRound(playerSelection, computerSelection) {
+
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
